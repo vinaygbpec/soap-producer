473175b69fc9f12309ae43694438de6bc76cf5b7
Rawat <889271@cognizant.com>
2021-05-05 15:38:00 +0530

    marshaling the object

--- src/main/java/com/oup/integration/endpoint/OTCEndpoint.java
+++ src/main/java/com/oup/integration/endpoint/OTCEndpoint.java
@@ -1,9 +1,13 @@
 package com.oup.integration.endpoint;
 
+import javax.xml.bind.JAXBException;
+
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
 import com.sap.document.sap.rfc.functions.ZTESTSCHEMARequest;
 import com.sap.document.sap.rfc.functions.ZTESTSCHEMAResponse;
 
@@ -15,6 +19,13 @@ public class OTCEndpoint {
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "ZTEST_SCHEMARequest")
 	@ResponsePayload
 	public ZTESTSCHEMAResponse submitOTCSchema(@RequestPayload ZTESTSCHEMARequest request) {
+		Jaxb2Marshaller jaxb2Marshaller=new Jaxb2Marshaller();
+		try {
+			jaxb2Marshaller.createMarshaller().marshal(request, System.out);
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		ZTESTSCHEMAResponse response = new ZTESTSCHEMAResponse();
 		response.setHttpStatus("OK");
 		response.setMessage("Succefully submitted.");
