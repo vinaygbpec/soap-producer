cc6d7959320c3664822e6fc1322ae53cac9f5599
vinaygbpec <vinay@pixeltechlab.com>
2021-05-05 16:33:55 +0530

    marshal created bean

--- src/main/java/com/oup/integration/endpoint/OTCEndpoint.java
+++ src/main/java/com/oup/integration/endpoint/OTCEndpoint.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -18,24 +19,19 @@ import com.sap.document.sap.rfc.functions.ZTESTSCHEMAResponse;
 
 @Endpoint
 public class OTCEndpoint {
+	@Autowired
+	private Marshaller marshaller;
 	private static final String NAMESPACE_URI = "urn:sap-com:document:sap:rfc:functions";
 
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "ZTEST_SCHEMARequest")
 	@ResponsePayload
 	public ZTESTSCHEMAResponse submitOTCSchema(@RequestPayload ZTESTSCHEMARequest request) throws JAXBException {
-		Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
-		jaxb2Marshaller.setPackagesToScan("com.sap.document.sap.rfc.functions");
-		Map<String, Object> properties = new HashMap<>();
-		properties.put(javax.xml.bind.Marshaller.JAXB_FORMATTED_OUTPUT, true);
-		jaxb2Marshaller.setMarshallerProperties(properties);
-
-		Marshaller marshaller = jaxb2Marshaller.createMarshaller();
 		marshaller.marshal(request, System.out);
 		File file = new File("IN/output");
 		marshaller.marshal(request, file);
 		ZTESTSCHEMAResponse response = new ZTESTSCHEMAResponse();
 		response.setHttpStatus("OK");
-		response.setMessage("Succefully submitted.");
+		response.setMessage("Succesfully submitted.");
 		return response;
 	}
 }
